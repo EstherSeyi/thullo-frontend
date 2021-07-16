2dd289c333cf8d980cfdf575ba8c21a384186ced
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-07-16 18:03:33 +0100

    integrates modal

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import 'tailwindcss/tailwind.css'
+import "../styles/globals.css";
+
+import { ModalProvider } from "../context/modal";
+import Modal from "../components/modal";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ModalProvider>
+      <Modal />
+      <Component {...pageProps} />
+    </ModalProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
