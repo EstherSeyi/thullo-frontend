a5b1c9d65c928cfebf9609726d5179f4502016c3
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-07-16 17:59:42 +0100

    adds modal action creators

---
+++ context/actions/modal.js
@@ -0,0 +1,15 @@
+export const registerComponent = (component) => ({
+  payload: {
+    modalComponent: component,
+    isModalOpen: true,
+  },
+  type: "REGISTER_COMPONENT",
+});
+
+export const unregisterComponent = () => ({
+  type: "UNREGISTER_COMPONENT",
+  payload: {
+    modalComponent: null,
+    isModalOpen: false,
+  },
+});
