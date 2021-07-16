ca4704d631cca4284d35813e398ac8fa794b9139
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-07-16 18:03:55 +0100

    modifies home page

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,75 @@
-import Head from 'next/head'
+import Image from "next/image";
+
+import Layout from "../components/layout";
+import AddBoard from "../components/add-board";
+
+import profilepic from "../public/profilepic.jpeg";
+
+import { useModal } from "../context/modal";
 
 export default function Home() {
+  const { open } = useModal();
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout>
+      <section className="w-11/12 max-w-[1024px] mx-auto mt-8 ">
+        <h1 className="text-lg">All Boards</h1>
+        <button
+          className="px-5 py-1.5 bg-blueish-250 rounded text-xs text-misc-white ml-auto block"
+          onClick={() => open(<AddBoard />)}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+          Add
+        </button>
+        <div className="grid gap-8 mt-16 grid-cols-200">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </section>
+    </Layout>
+  );
 }
+
+const Card = () => {
+  return (
+    <div className="bg-misc-white p-3 rounded-md shadow-lg">
+      <div className="bg-cardpics px-20 py-16 rounded-md"></div>
+      <p className="mt-2.5 mb-4">Devchallenges Board</p>
+      <div className="flex">
+        <div className="flex">
+          <div className="mr-2">
+            <Image
+              className="rounded"
+              src={profilepic}
+              alt="avatar"
+              width={25}
+              height={25}
+            />
+          </div>
+          <div className="mr-2">
+            <Image
+              className="rounded"
+              src={profilepic}
+              alt="avatar"
+              width={25}
+              height={25}
+            />
+          </div>
+          <div className="mr-2">
+            <Image
+              className="rounded"
+              src={profilepic}
+              alt="avatar"
+              width={25}
+              height={25}
+            />
+          </div>
+        </div>
+        <span className="font-noto text-greyish-150 text-xs self-center">
+          + 5 others
+        </span>
+      </div>
+    </div>
+  );
+};
