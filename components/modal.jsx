b07c0aaa0769ef67375362d3b0b482a8fb8e6c56
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-07-16 17:58:57 +0100

    adds modal wrapper component

---
+++ components/modal.jsx
@@ -0,0 +1,38 @@
+import { useRef, useEffect } from "react";
+
+import { useModal } from "../context/modal";
+
+const Modal = () => {
+  const { modalState, close } = useModal();
+
+  const modalRef = useRef();
+
+  const handleClick = (event) => {
+    if (modalRef?.current?.contains(event.target)) {
+      return;
+    }
+    close();
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  });
+
+  return (
+    <div
+      className={`fixed h-screen w-screen top-0 left-0 overflow-auto bg-misc-black  z-10 bg-opacity-50 ${
+        modalState?.isModalOpen ? "block" : "hidden"
+      }`}
+    >
+      <div ref={modalRef} className="w-11/12 lg:w-2/3 mx-auto">
+        {modalState?.modalComponent}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
