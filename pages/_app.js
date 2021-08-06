7e37fb905010d87049b1b969ec683f52aa511332
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-08-07 00:26:06 +0100

    updates app entry

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
+import { useRouter } from "next/router";
+
 import "../styles/globals.css";
 
 import { ModalProvider } from "../context/modal";
 import Modal from "../components/modal";
+import Layout from "../components/layout";
 
 function MyApp({ Component, pageProps }) {
+  const { query } = useRouter();
   return (
     <ModalProvider>
       <Modal />
-      <Component {...pageProps} />
+      <Layout cardName={query.id}>
+        <Component {...pageProps} />
+      </Layout>
     </ModalProvider>
   );
 }
