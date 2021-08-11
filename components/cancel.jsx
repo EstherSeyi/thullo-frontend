cd349a7f51c9aa94eca0d1c99e5b3f672d51bc43
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-08-11 19:15:28 +0100

    adds new icons

--- components/cancel.jsx
+++ components/cancel.jsx
@@ -1,11 +1,11 @@
-const Cancel = (props) => {
+const Cancel = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
-      {...props}
     >
       <path
         strokeLinecap="round"
