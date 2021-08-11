4c45bbbd3f403d77cb7015cf1ece4464368b4dea
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-08-11 19:14:36 +0100

    updates Add Another component

--- components/add-another.jsx
+++ components/add-another.jsx
@@ -1,9 +1,11 @@
 import Plus from "./icons/plus";
 
-const AddAnother = ({ text, classes = "" }) => {
+const AddAnother = ({ text, classes = "", onClick }) => {
   return (
     <div
       className={`flex justify-between font-noto text-xs bg-blueish-150 text-blueish-250 items-center p-1 rounded-md px-1.5 ${classes}`}
+      role="button"
+      onClick={onClick}
     >
       <p>{text}</p>
       <Plus />
