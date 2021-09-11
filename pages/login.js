601faf620653b69b6e7ce5a681f1d199090c4d80
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-09-11 17:12:51 +0100

    updates login

--- pages/login.js
+++ pages/login.js
@@ -1,40 +1,38 @@
-import Image from "next/image";
-
-import logo from "../public/Logo-opp.svg";
+import Input from "../components/input";
+import Button from "../components/auth-button";
+import GooglePrompt from "../components/google-prompt";
+import Or from "../components/or";
 
 const Login = () => {
   return (
-    <section className="flex min-h-screen">
-      <div className="hidden sm:block  bg-blueish-250  flex-30">
-        <div className="mt-[20%] mx-auto w-max">
-          <Image src={logo} alt="logo" />
-        </div>
+    <form className="w-11/12 mx-auto max-w-[400px]">
+      <Input label="Username or email" />
+      <Input label="Password" />
 
-        <div className="text-misc-white mt-8 w-11/12 mx-auto flex items-center flex-col">
-          <p className="text-center">
-            Click button to generate test user credentials
-          </p>
-          <button className="mx-auto text-blueish-250 rounded-md text-xs px-5 py-1.5 bg-blueish-50 mt-4 shadow-md ">
-            Click me!
-          </button>
-          <div className="mt-10 bg-blueish-50 text-blueish-250 p-4 rounded">
-            <p>
-              <span className="capitalize">username: </span>{" "}
-              <span>mostproductive</span>
-            </p>
-            <p>
-              <span className="capitalize">password: </span>{" "}
-              <span>logmein</span>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="w-full sm:flex-70 bg-blueish-50 ">
-        <form>
-          <input placeholder="username" name="username" />
-        </form>
-      </div>
-    </section>
+      <a
+        className="w-max block ml-auto text-0.625rem font-noto font-thin text-blueish-250"
+        href="#"
+      >
+        Forgot Password ?
+      </a>
+      <Button>Login</Button>
+
+      <Or />
+
+      <GooglePrompt text="Sign in with google." />
+
+      <p className="text-0.625rem font-noto font-thin text-center mt-6 text-greyish-100">
+        <span>First time here ?</span>
+        <a className="text-blueish-250 ml-0.5 hover:underline">
+          Login as test user
+        </a>{" "}
+        <span>or</span>
+        <a className="text-blueish-250 ml-0.5 hover:underline" href="/register">
+          Register
+        </a>
+        .
+      </p>
+    </form>
   );
 };
 
