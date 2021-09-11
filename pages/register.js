6c68a91aabdb4c8e6205df4386f99bc950e9a2e7
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-09-11 17:13:00 +0100

    updates register

--- pages/register.js
+++ pages/register.js
@@ -1,8 +1,29 @@
+import Button from "../components/auth-button";
+import Input from "../components/input";
+import GooglePrompt from "../components/google-prompt";
+import Or from "../components/or";
+
 const Register = () => {
   return (
-    <>
-      <p>Register</p>
-    </>
+    <form className="w-11/12 mx-auto max-w-[400px]">
+      <Input label="Username" />
+      <Input label="Email" />
+      <Input label="Password" type="password" />
+      <Input label="Confirm Password" type="password" />
+
+      <Button> Register</Button>
+
+      <Or />
+      <GooglePrompt text="Sign up with google." />
+
+      <p className="text-0.625rem font-noto font-thin text-center mt-6 text-greyish-100">
+        <span>Have an account ?</span>
+        <a className="text-blueish-250 ml-0.5 hover:underline" href="/login">
+          Login
+        </a>{" "}
+        .
+      </p>
+    </form>
   );
 };
 
