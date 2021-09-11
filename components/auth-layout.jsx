27caebd7a7c12d9077e813b63700e16236a977c9
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-09-11 17:11:55 +0100

    adds auth layout component

---
+++ components/auth-layout.jsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+import logo from "../public/Logo.svg";
+import collaboration from "../public/collaboration.svg";
+import googleLogo from "../public/google-logo.png";
+
+const AuthLayout = ({ children }) => {
+  return (
+    <section className="flex min-h-screen">
+      <div className="hidden md:block  bg-blueish-250  flex-50">
+        <div className="text-misc-white mt-8 w-11/12 mx-auto flex items-center flex-col">
+          <Image src={collaboration} alt="logo" width={400} />
+          <p className="font-noto w-6/12 text-center font-thin">
+            Provding easy and flexible ways to manage your project
+          </p>
+        </div>
+      </div>
+      <div className="w-full md:flex-50 bg-blueish-50">
+        <div className="mt-24">
+          <div className="mx-auto w-max mb-16">
+            <Image src={logo} alt="logo" />
+          </div>
+          {children}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AuthLayout;
