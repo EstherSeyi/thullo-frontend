355e032fae037eb8684ef9303aae3fd39c0bc327
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-09-11 17:11:44 +0100

    adds auth-button component

---
+++ components/auth-button.jsx
@@ -0,0 +1,14 @@
+const Button = ({ children, rest }) => {
+  return (
+    <div className="flex justify-center mt-12">
+      <button
+        className="bg-blueish-250 px-6 py-1.5 rounded-lg text-misc-white border border-blueish-250 hover:bg-transparent hover:text-blueish-250"
+        {...rest}
+      >
+        {children}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
