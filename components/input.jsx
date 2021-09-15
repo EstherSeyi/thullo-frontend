5707360b2641997d25603229c1ec73db127c2922
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-09-15 15:25:21 +0100

    updates input component

--- components/input.jsx
+++ components/input.jsx
@@ -1,13 +1,18 @@
-const Input = ({ label, rest }) => {
+const Input = ({ label, errored, errorMessage, ...rest }) => {
   return (
-    <label>
+    <label className="relative block mb-8">
       <span className="font-noto font-thin text-greyish-100 text-xs block mb-3">
         {label}
       </span>
       <input
-        className="w-full bg-transparent border-b border-greyish-150 mb-6 focus:outline-none focus:border-yellowish-100 text-greyish-200"
+        className="w-full bg-transparent border-b border-greyish-150  focus:outline-none focus:border-yellowish-100 text-greyish-200"
         {...rest}
       />
+      {errored ? (
+        <small className="text-misc-red absolute left-0 font-thin text-0.625rem top-14 truncate">
+          {errorMessage}
+        </small>
+      ) : null}
     </label>
   );
 };
