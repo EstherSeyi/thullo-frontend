718588c1fbb53765d2892532e84abed5fb9a9d04
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-09-15 15:25:07 +0100

    updates auth-layout component

--- components/auth-layout.jsx
+++ components/auth-layout.jsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 import logo from "../public/Logo.svg";
 import collaboration from "../public/collaboration.svg";
 import googleLogo from "../public/google-logo.png";
+import { useRouter } from "next/router";
 
 const AuthLayout = ({ children }) => {
+  const router = useRouter();
   return (
     <section className="flex min-h-screen">
       <div className="hidden md:block  bg-blueish-250  flex-50">
@@ -17,7 +19,10 @@ const AuthLayout = ({ children }) => {
       </div>
       <div className="w-full md:flex-50 bg-blueish-50">
         <div className="mt-24">
-          <div className="mx-auto w-max mb-16">
+          <div
+            className="mx-auto w-max mb-16 cursor-pointer"
+            onClick={() => router.push("/")}
+          >
             <Image src={logo} alt="logo" />
           </div>
           {children}
