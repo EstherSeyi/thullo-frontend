0d18804512129d3ddb6e6a8408b2dc2558fc49c3
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-09-15 15:24:47 +0100

    updates button component with loading activity

--- components/auth-button.jsx
+++ components/auth-button.jsx
@@ -1,11 +1,11 @@
-const Button = ({ children, rest }) => {
+const Button = ({ children, loading, ...rest }) => {
   return (
     <div className="flex justify-center mt-12">
       <button
         className="bg-blueish-250 px-6 py-1.5 rounded-lg text-misc-white border border-blueish-250 hover:bg-transparent hover:text-blueish-250"
         {...rest}
       >
-        {children}
+        {loading ? "Loading..." : children}
       </button>
     </div>
   );
