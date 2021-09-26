04629862b36ea9e5f18b19703501362ae74f5364
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-09-26 07:11:47 +0100

    updates boards component

--- pages/boards.js
+++ pages/boards.js
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { useInfiniteQuery } from "react-query";
+import queryString from "query-string";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 import AddBoard from "../components/board-actions/add-board";
 import MemberAvatar from "../components/member-avatar";
@@ -8,16 +12,42 @@ import coverpic from "../public/cardpics.jpeg";
 
 import { useModal } from "../context/modal";
 import { useUser } from "../hooks/auth-hook";
-import { useAppQuery } from "../hooks/query-hook";
+import request from "../helpers/request";
+import { Fragment } from "react";
 
 export default function Home() {
   const { open } = useModal();
 
   const { user } = useUser();
 
-  const { data } = useAppQuery(`boards_${user?.id}`, {
-    url: `/boards?_where[members.username_contains]=${user?.username}`,
-  });
+  const {
+    data: infinitData,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery(
+    `boards_${user?.id}`,
+    ({ pageParam = 0 }) => {
+      return request.get(
+        `/boards?_where[members.username_contains]=${user?.username}&_limit=8&_start=${pageParam}`
+      );
+    },
+    {
+      getNextPageParam: (lastPage) => {
+        let parsed = queryString.parse(lastPage.config.url);
+
+        parsed._start = Number(parsed._start) + 8;
+        return parsed._start;
+      },
+      getPreviousPageParam: (lastPage) => {
+        let parsed = queryString.parse(lastPage.config.url);
+        parsed._start =
+          Number(parsed._start) > 0 ? Number(parsed._start) - 8 : 0;
+
+        return parsed._start;
+      },
+    }
+  );
 
   return (
     <section className="w-11/12 max-w-[1024px] mx-auto mt-8 ">
@@ -28,34 +58,71 @@ export default function Home() {
       >
         Add
       </button>
-      {data?.length ? (
-        <div className="flex flex-wrap mt-16">
-          {data?.map((board) => (
-            <Card
-              key={board?.id}
-              id={board?.id}
-              title={board?.title}
-              members={board?.members}
-              cover_image={
-                board?.cover_photo?.formats?.small?.url
-                  ? `${process.env.NEXT_PUBLIC_BASE_URL}${board?.cover_photo?.formats?.small?.url}`
-                  : coverpic
-              }
-            />
-          ))}
-        </div>
-      ) : (
-        <p className="text-sm font-noto font-thin text-center mt-20 text-greyish-100">
-          No boards Yet? Click the "Add" button to create one!
-        </p>
-      )}
+      <div className="h-main overflow-y-scroll" id="unsplash-image-thumbnails">
+        {infinitData?.pages?.length ? (
+          <InfiniteScroll
+            dataLength={infinitData?.pages?.length}
+            hasMore={hasNextPage}
+            loader={
+              isFetchingNextPage ? (
+                <p className="text-center italic">Loading...</p>
+              ) : null
+            }
+            next={fetchNextPage}
+            scrollableTarget="unsplash-image-thumbnails"
+            endMessage={
+              <p
+                style={{
+                  textAlign: "center",
+                  marginTop: "1em",
+                  fontWeight: "bold",
+                }}
+              >
+                You have seen it all!
+              </p>
+            }
+          >
+            <div
+              className={`mt-16 grid ${
+                infinitData?.pages[0]?.data?.length > 1 &&
+                infinitData?.pages[0]?.data?.length < 3
+                  ? "grid-cols-automax"
+                  : "grid-cols-200"
+              }  gap-4
+  `}
+            >
+              {infinitData?.pages?.map((page, index) => (
+                <Fragment key={`board-pages-${index}`}>
+                  {page?.data?.map((board) => (
+                    <Card
+                      key={board?.id}
+                      id={board?.id}
+                      title={board?.title}
+                      members={board?.members}
+                      cover_image={
+                        board?.cover_photo?.formats?.small?.url
+                          ? `${process.env.NEXT_PUBLIC_BASE_URL}${board?.cover_photo?.formats?.small?.url}`
+                          : coverpic
+                      }
+                    />
+                  ))}
+                </Fragment>
+              ))}
+            </div>
+          </InfiniteScroll>
+        ) : (
+          <p className="text-sm font-noto font-thin text-center mt-20 text-greyish-100">
+            No boards Yet? Click the "Add" button to create one!
+          </p>
+        )}
+      </div>
     </section>
   );
 }
 
-const Card = ({ title, members, cover_image }) => {
+const Card = ({ title, members, cover_image, id }) => {
   return (
-    <div className="bg-misc-white p-3 rounded-md shadow-lg mr-8 mb-8 w-full max-w-[200px]">
+    <div className="bg-misc-white p-3 rounded-md shadow-lg w-full xs:w-11/12 sm:w-full mx-auto sm:max-w-[250px]">
       <div className="rounded-md w-full">
         <Image
           className="rounded"
@@ -67,8 +134,8 @@ const Card = ({ title, members, cover_image }) => {
           layout="responsive"
         />
       </div>
-      <Link href={`/boards/${title}`} className="block mt-2.5 mb-4">
-        {title}
+      <Link href={`/boards/${title}?docId=${id}`}>
+        <p className="block mt-4 mb-4 truncate cursor-pointer">{title}</p>
       </Link>
       <div className="flex">
         <div className="flex">
