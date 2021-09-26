27c3723f2fc3ca08d8a1cd1c965fbd7c61ef54d8
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-09-26 13:29:28 +0100

    updates member row component

--- components/board-actions/member-row.jsx
+++ components/board-actions/member-row.jsx
@@ -1,17 +1,28 @@
 import MemberAvatar from "../member-avatar";
 
-const MemberRow = ({ name, admin = false, imageSrc }) => {
+const MemberRow = ({
+  name,
+  admin = false,
+  imageSrc,
+  alt,
+  handleRemoveUser,
+  userId,
+  removeLoading,
+}) => {
   return (
     <div className="flex items-center justify-between font-poppins mb-4">
       <div className="flex items-center">
-        <MemberAvatar imgSrc={imageSrc} />
+        <MemberAvatar imgSrc={imageSrc} alt={alt} />
         <p className="ml-4 text-misc-black font-bold">{name}</p>
       </div>
       {admin ? (
         <p className="text-greyish-150 text-0.625rem">Admin</p>
       ) : (
-        <button className="text-misc-red py-1 px-2 border border-misc-red rounded-md text-0.625rem">
-          Remove
+        <button
+          className="text-misc-red py-1 px-2 border border-misc-red rounded-md text-0.625rem"
+          onClick={() => handleRemoveUser(userId)}
+        >
+          {removeLoading ? "Loading..." : "Remove"}
         </button>
       )}
     </div>
